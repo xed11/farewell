a9924cd33427316913c46f0c01c60930b09167e5
xed11 <demesa.dexter@gmail.com>
2026-08-08 08:53:55 +0100

    Update farewell messages for Jessica

--- messages.js
+++ messages.js
@@ -4,14 +4,24 @@
  */
 window.FAREWELL_MESSAGES = [
   {
+    name: "Sandra",
+    lang: "en",
+    text: "Obrigada jessica por tudo .foste e ès uma pessoa ecrivel e meiga espero tudo de bom nesta tua nova etapa da tua vida . I will miss you so much❤️ - Sandra😘😘",
+  },
+  {
+    name: "Dexter",
+    lang: "en",
+    text: "❤️😃 To our dearest Jessica, it's a mix emotions knowing that you're leaving🥹🥹. Remembering how good our night routine was, and how we perform our role as a team. I'm always grateful and thankful that you were our leader, our senior carer, and a friend for the past three years. Your positivity❤️, kindness, thoughfullness, cheerfulness, etc.. are amazing!❤️ You will be missed.. Congratulations! and good luck in this new chapter of your career. Keep in touch, and enjoy the new environment.. God Bless and we'll see you again.❤️❤️❤️❤️",
+  },
+  {
     name: "Marcia",
     lang: "en",
     text: "Jessica❤️ it's been a pleasure working with you, and you'll be greatly missed. Thank you for your kindness, support, and all the memories we've shared. While sad to see you go but also excited for your new opportunity. Wishing you every success and happiness in your new role—best of luck for the future! Ahpequenaaaaa😘 Marcia",
   },
   {
-    name: "Sandra",
+    name: "Ewelina",
     lang: "en",
-    text: "Obrigada jessica por tudo .foste e ès uma pessoa ecrivel e meiga espero tudo de bom nesta tua nova etapa da tua vida . I will miss you so much❤️ - Sandra😘😘",
+    text: "Congratulations on your new job! Although we’ll miss you, we’re excited to see you begin this new journey. You deserve every success and happiness that comes your way. Thank you for your kindness, support, and friendship. Wishing you all the best for the future – go and shine! - Ewelina 😅",
   },
   {
     name: "Ana",
@@ -39,9 +49,19 @@ window.FAREWELL_MESSAGES = [
     text: "Dear Jessica. It has been such a pleasure working with you and I will always remember our time together. Wishing you all the best in your new job and looking forward to keeping in touch. Take care! - Lorna xxxx",
   },
   {
-    name: "Dex",
-    lang: "en",
-    text: "❤️😃 Hello Jessica, it's a mix emotions knowing that you're leaving🥹🥹. Remembering how good our night routine was, and how well we understand each other, how each of us worked, and how we perform our role as a team. I'm always grateful and thankful that you were our leader, our senior carer, and a friend for the past three years. Your positivity❤️, kindness, thoughfullness, cheerfulness, etc, etc.. are overflowing..❤️ It's really hard to let you go.. Good luck in this new chapter of your career. Keep in touch, and stay the same. We're all gonna missed you. God Bless and we'll see you again.❤️❤️❤️❤️",
+    name: "De Susana",
+    lang: "pt",
+    text: "Farewell Jessica! 🌸 Wishing you all the best for the future and lots of happiness and success. Take care and good luck! - De Susana ❤️",
+  },
+  {
+    name: "Lucia Ramos",
+    lang: "pt",
+    text: "Jessica. A vida é feita de ciclos,escolhas ,desafios e oportunidades☺️☺️ Deixo aqui o meu...agradecimento por ter o prazer de trabalhar contigo 🥰😍💕❤️💗💝💖❤️‍🩹💞 E claro.....não posso deixar de desejar Muito sucesso na tua nova jornada 🤪🤪🤩🤩 Com muito carinho 🤩🤩e saudades 🫂🫂🫂 - Lucia Ramos  \nPs...com estes corações todos parece que estou apaixonada por ti🤣🤣😂😂 Mas não fiques babando que não é paixão 🫣🫣",
+  },
+  {
+    name: "Lehrie",
+    lang: "pt",
+    text: "Hi Jess, wishing you every success, happiness, and fulfilment in whatever comes next. May this new journey brings you exciting opportunities, good health, and countless wonderful memories for you and your family. We will surely miss here in St. Joseph.— Lehrie",
   },
 ];
 
